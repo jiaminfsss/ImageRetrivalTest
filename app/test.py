91c000db2bf01a27efa62eee307d0692545f1b42
jiaminfsss <893611119@qq.com>
2022-12-25 16:01:28 +0800

    完成了从html页面向Flask脚本传递表单的功能

--- app/test.py
+++ app/test.py
@@ -1,9 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template('index.html')
+
+@app.route("/searchByText",methods=['POST'])
+def searchByText():
+    searchText = request.form['searchText']
+    print("当前搜索的图片是："+searchText)
     return render_template('index.html')
 
 if __name__ == "__main__":
