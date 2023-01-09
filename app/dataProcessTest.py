263908455ae69df8cbfe07799ebd501c1c5bde54
jiaminfsss <893611119@qq.com>
2023-01-09 17:00:25 +0800

    完成了根据检索结果在前端显示图片的功能，但瀑布流还未开发完善，上下图片之间仍然有间距

--- app/dataProcessTest.py
+++ app/dataProcessTest.py
@@ -3,6 +3,7 @@ import os
 import torch
 import clip       
 from PIL import Image
+import json
 
 #socket通信设置
 imageProcess_server = socket(AF_INET,SOCK_STREAM)
@@ -11,9 +12,11 @@ imageProcess_server.bind(address)
 
 imagePath = '/home/fusong/DataImage/test2/'
 
+
 #载入模型
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
+
 
 #遍历图片
 def getImage(imagePath):
@@ -26,21 +29,35 @@ while (1):
 
     client_socket, clientAddr = imageProcess_server.accept()
     searchText = client_socket.recv(1024).decode("gbk")
-    print("接收的数据："+searchText)
+    print("接收的数据："+searchText+" Client addrs is: "+str(clientAddr))
 
-    send_data = client_socket.send("我接受到的数据为：".encode("gbk")+searchText.encode("gbk"))
+    send_data = client_socket.send(str(clientAddr[1]).encode("gbk"))
     client_socket.close()
+    
     
     #模型处理图片和文字
     text = clip.tokenize([searchText, "a diagram", "a car", "a rabbit"]).to(device)
     text_features = model.encode_text(text)
     imageList = getImage(imagePath)#得到图片库中的图片List
-    for i in imageList:
-        image = preprocess(Image.open(imagePath+i)).unsqueeze(0).to(device)
-        with torch.no_grad():
-            image_features = model.encode_image(image)
-            logits_per_image, logits_per_text = model(image, text)
-            probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-            print("Label of image "+i+" probs:", probs)
-
+    
+    with open("/home/fusong/DataImage/searchBTResult/"
+              +searchText+str(clientAddr[1])+".json", 'a') as f:
+        f.write("[")
+        count = 0
+        for i in imageList:
+            image = preprocess(Image.open(imagePath+i)).unsqueeze(0).to(device)
+            with torch.no_grad():
+                image_features = model.encode_image(image)
+                logits_per_image, logits_per_text = model(image, text)
+                probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+                if (probs[0][0]>0.8):
+                    print("Label of image "+i+" probs:", probs[0][0])
+                    if(count == 1):
+                        f.write(", \""+i+"\"")
+                    else:
+                        f.write(" \""+i+"\"")
+                        count = 1
+        f.write(" ]")
+        f.close()
+    
 
--- app/test.py
+++ app/test.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request, send_from_directory, send_file, json, jsonify, make_response
 import requests
 from socket import *
 
@@ -11,17 +11,40 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/searchByText",methods=['POST'])
+@app.route("/searchResult/")
+def searchResult():
+    return render_template("searchResult.html")
+
+@app.route("/getResult/<file_name>", methods=['GET'])
+def getResult(file_name):
+    directory = "/home/fusong/DataImage/searchBTResult/"
+    try:
+        response = make_response(
+            send_from_directory(directory, file_name, as_attachment=True))
+        print(response)
+        return response
+    except Exception as e:
+        return jsonify({"code": "异常", "message": "{}".format(e)})
+
+@app.route("/getImage/<file_name>", methods=['GET'])
+def getImage(file_name):
+    directory = "/home/fusong/DataImage/test2/"
+    return send_file(directory+file_name, mimetype='image/gif')
+    
+@app.route("/searchByText", methods=['POST'])
 def searchByText():
     searchText = request.form['searchText']
     print("当前搜索的图片是："+searchText)
     sendSocket = socket(AF_INET,SOCK_STREAM)
     sendSocket.connect(('',imageServerPort))
     sendSocket.send(searchText.encode("gbk"))
-    from_imageServer_msg = sendSocket.recv(1024)
-    print("从服务器接收到的数据是："+from_imageServer_msg.decode("gbk"))
+    portNum = sendSocket.recv(1024).decode("gbk")
+    print("从服务器接收到的数据是："+portNum)
     sendSocket.close()
-    return render_template('index.html')
+    return render_template('searchResult.html', 
+                           searchText=searchText, 
+                           communicatePort=portNum
+                           )
 
 if __name__ == "__main__":
     app.run()
